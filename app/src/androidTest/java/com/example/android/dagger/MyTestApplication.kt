97b82c1eb37ef0f69b47f607f2c80a8382cf4b07
AlexeyAnurin <Anurin.Alexey@gmail.com>
2021-02-22 22:44:58 +0200

    refactor

--- app/src/androidTest/java/com/example/android/dagger/MyTestApplication.kt
+++ app/src/androidTest/java/com/example/android/dagger/MyTestApplication.kt
@@ -16,16 +16,10 @@
 
 package com.example.android.dagger
 
-import com.example.android.dagger.di.AppComponent
-import com.example.android.dagger.di.DaggerTestAppComponent
 
 /**
  * MyTestApplication will override MyApplication in android tests
  */
 class MyTestApplication : MyApplication() {
 
-    override fun initializeComponent(): AppComponent {
-        // Creates a new TestAppComponent that injects fakes types
-        return DaggerTestAppComponent.create()
     }
-}
