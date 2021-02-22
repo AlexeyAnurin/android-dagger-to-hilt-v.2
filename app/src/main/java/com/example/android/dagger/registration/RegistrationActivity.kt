bb3e25f1f8693ca5597380197d5436e3a1ce434a
AlexeyAnurin <Anurin.Alexey@gmail.com>
2021-02-22 21:45:12 +0200

    @AndroidEntryPoint RegistrationActivity, auxiliary  @EntryPoint code deleted

--- app/src/main/java/com/example/android/dagger/registration/RegistrationActivity.kt
+++ app/src/main/java/com/example/android/dagger/registration/RegistrationActivity.kt
@@ -26,33 +26,19 @@ import com.example.android.dagger.registration.enterdetails.EnterDetailsFragment
 import com.example.android.dagger.registration.termsandconditions.TermsAndConditionsFragment
 import dagger.hilt.EntryPoint
 import dagger.hilt.InstallIn
+import dagger.hilt.android.AndroidEntryPoint
 import dagger.hilt.android.EntryPointAccessors
 import dagger.hilt.android.components.ApplicationComponent
 import javax.inject.Inject
 
+@AndroidEntryPoint
 class RegistrationActivity : AppCompatActivity() {
-
-    @InstallIn(ApplicationComponent::class)
-    @EntryPoint
-    interface RegistrationEntryPoint {
-        fun registrationComponent(): RegistrationComponent.Factory
-    }
-
-    // Stores an instance of RegistrationComponent so that its Fragments can access it
-    lateinit var registrationComponent: RegistrationComponent
 
     // @Inject annotated fields will be provided by Dagger
     @Inject
     lateinit var registrationViewModel: RegistrationViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
-        // Creates an instance of Registration component by grabbing the factory from the app graph
-        val entryPoint = EntryPointAccessors.fromApplication(applicationContext, RegistrationEntryPoint::class.java)
-        registrationComponent = entryPoint.registrationComponent().create()
-
-        // Injects this activity to the just created Registration component
-        registrationComponent.inject(this)
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
