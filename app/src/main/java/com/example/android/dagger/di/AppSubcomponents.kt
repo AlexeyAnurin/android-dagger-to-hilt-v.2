7ea762da9a45a8b6f1bbca7d145f356b703a8836
AlexeyAnurin <Anurin.Alexey@gmail.com>
2021-02-22 21:42:27 +0200

    LoginComponent deleted

--- app/src/main/java/com/example/android/dagger/di/AppSubcomponents.kt
+++ app/src/main/java/com/example/android/dagger/di/AppSubcomponents.kt
@@ -16,7 +16,6 @@
 
 package com.example.android.dagger.di
 
-import com.example.android.dagger.login.LoginComponent
 import com.example.android.dagger.registration.RegistrationComponent
 import com.example.android.dagger.user.UserComponent
 import dagger.Module
